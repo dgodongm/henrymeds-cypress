3ce7c8dffa084f54fae53f132c6f27ebcd96bfd6
Demian Godon <demiangodon@Demians-MacBook-Air.local>
2024-05-05 18:50:10 -0700

    first pass test refactoring to improve readability and maintenance

--- cypress/tests/ui/appointment.spec.js
+++ cypress/tests/ui/appointment.spec.js
@@ -1,38 +1,49 @@
-describe('appointment test spec', () => {
-  it('Partial appointment flow', () => {
-    // Test going up to payment screen
+import { valid_users } from '../../fixtures/valid_users'
+import { valid_states } from '../../fixtures/valid_states'
+import {
+  verifyAppointmentLandingPage,
+  verifyAvailableSlotsPage,
+  verifyNextStepsPage,
+  verifyPaymentPage,
+  verifyShippingPage,
+} from '../../support/utils'
 
+describe('appointment test spec', () => {
+  beforeEach(() => {
     // Go to appointment landing page
     cy.visit('?override_kameleoon')
+  })
+
+  it('Partial appointment flow', () => {
+    // Test appointment booking flow for Washington going up to payment screen
+
     // check appointment landing page
-    cy.get('h2').contains('Schedule your Appointment!')
-    cy.get('p').contains('What state do you live in?')
-    cy.get('div.MuiStack-root').within(() => {
-      cy.get('button')
-        .should('have.length', 14)
-        .then((list) => Cypress._.map(list, 'innerText'))
-        .should((list) => {
-          const just_states = list.slice(0, list.length - 1)
-          const sorted_states = just_states.toSorted()
-          expect(just_states).to.be.deep.equal(sorted_states)
-        })
-    })
+    verifyAppointmentLandingPage()
 
     // select state
-    cy.getByData('washington').click()
+    cy.getByData(valid_states[valid_states.length - 1].testid).click()
 
     // check that there are available appointment slots and select first one
-    cy.get('h2').contains('Next Available Time')
-    cy.get('p').contains('Select a time to meet with a licensed provider about')
+    verifyAvailableSlotsPage()
     cy.get('div.MuiStack-root')
       .first()
       .within(() => {
-        cy.get('button').should('have.length.gt', 0).first().click()
+        cy.get('button')
+          .first()
+          .invoke('text')
+          .then((slotTime) => {
+            cy.log('First slot time: ' + slotTime)
+            const firstSlotTime = slotTime
+            cy.wrap(firstSlotTime).as('firstSlotTime')
+          })
+        cy.get('button').first().click()
       })
 
     // check "pending appointment/next steps" page
-    cy.get('p').contains('You have a pending appointment for') // TODO: also check time, date, provider, and maybe even profile image
-    cy.get('h1').contains('Next Steps')
+    cy.get('p').contains('You have a pending appointment for')
+    cy.get('@firstSlotTime').then((selectedSlotTime) => {
+      verifyNextStepsPage(selectedSlotTime)
+    })
 
     // click Continue
     cy.getByData('appointmentOverviewContinue').click()
@@ -41,31 +52,26 @@ describe('appointment test spec', () => {
     cy.get('h3').contains('Contact Details')
 
     // fill out contact details and click Continue
-    cy.getByData('firstName').type('Demian')
-    cy.getByData('lastName').type('Godon')
-    cy.getByData('email').type('dman@goforth.com')
-    cy.getByData('verifyEmail').type('dman@goforth.com')
-    cy.getByData('dob').type('01/20/1975')
-    cy.getByData('phoneNumber').type('206-675-9999')
-    cy.getByData('sex').select('Male')
+    cy.getByData('firstName').type(valid_users[0].firstName)
+    cy.getByData('lastName').type(valid_users[0].lastName)
+    cy.getByData('email').type(valid_users[0].email)
+    cy.getByData('verifyEmail').type(valid_users[0].email)
+    cy.getByData('dob').type(valid_users[0].dob)
+    cy.getByData('phoneNumber').type(valid_users[0].phoneNumber)
+    cy.getByData('sex').select(valid_users[0].sex)
     cy.getByData('tosConsent').check()
     cy.getByData('contactDetailsContinue').click()
 
     // check shipping page
-    cy.get('h3').contains('Shipping')
-    cy.getByData('state').should('be.visible').and('be.disabled') // TODO: check that previously selected state is displayed in disabled drop-down
+    verifyShippingPage()
 
     // fill out shipping info and click Continue
-    cy.getByData('addressLine1').type('701 N 34th Street')
-    cy.getByData('city').type('Seattle')
-    cy.getByData('zip').type('98103')
+    cy.getByData('addressLine1').type(valid_users[0].addressLine1)
+    cy.getByData('city').type(valid_users[0].city)
+    cy.getByData('zip').type(valid_users[0].zip)
     cy.getByData('shippingAddressContinue').click()
 
     // check payment screen
-    cy.get('h3').contains('Payment Method')
-    cy.get('form').within(() => {
-      cy.get('[id*="card"]').contains('Card Number')
-    })
-    cy.getByData('startTreatment').should('be.enabled')
+    verifyPaymentPage(valid_users[0].firstName, valid_users[0].lastName)
   })
 })
